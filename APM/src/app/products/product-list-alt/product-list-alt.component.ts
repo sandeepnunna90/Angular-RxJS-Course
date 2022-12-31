392c9028503eaaff033570c227560c34673af095
Sandeep Nunna <snunna@centrak.com>
2022-12-31 02:06:52 -0500

    Reacting to error

--- APM/src/app/products/product-list-alt/product-list-alt.component.ts
+++ APM/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -1,28 +1,29 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 
-import { catchError, EMPTY, Subscription } from 'rxjs';
+import { catchError, EMPTY, Subject, Subscription } from 'rxjs';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 
 @Component({
   selector: 'pm-product-list',
-  templateUrl: './product-list-alt.component.html'
+  templateUrl: './product-list-alt.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+  // Only detects changes made to input properties and events from child components
+  // and observables bound in the template using asyn pipe
+  // Bound values set in local properties won't trigger change detection so won't update the UI (like errorMessage here)
 })
 export class ProductListAltComponent {
   pageTitle = 'Products';
-  errorMessage = '';
-  selectedProductId = 0;
-
-  products: Product[] = [];
-  sub!: Subscription;
+  private errorMessageSubject = new Subject<string>();
+  errorMessage$ = this.errorMessageSubject.asObservable();
 
   constructor(private productService: ProductService) { }
 
   products$ = this.productService.productsWithCategory$
     .pipe(
       catchError(err => {
-        this.errorMessage = err;
+        this.errorMessageSubject.next(err);
         return EMPTY; // or return of([])
       })
     );
